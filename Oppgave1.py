380066967df0fe2910963b4729353baacecd2614
ffredz <fredrikscheie@live.no>
2026-02-09 10:06:49 +0100

    hgkiyliu

--- Oppgave1.py
+++ Oppgave1.py
@@ -1,10 +1,10 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-#1.1
+#1.3 a
 
 G = 9.81 #m/s^2
-
+init_values = [.1,.5,1]
 
 def diffeq(g,l):
     return 
@@ -21,8 +21,65 @@ def euler(theta_0,n,h,l):
 
     return theta,t
 
-ting = euler(.1,1000,.001,.1)
-theta = ting[0]
-t = ting[1]
-plt.plot(t,theta)
+def analytisk(theta_0,n,l,h):
+    w0 = np.sqrt(G/l)
+    t = np.arange(0,n*h,h)
+    theta = np.zeros(n)
+    theta[0]=theta_0
+    for i in range(1,len(theta)):
+        theta[i]=theta_0*np.cos(w0*t[i])
+    return theta,t
+
+
+for k in range(len(init_values)):
+    stuff = euler(init_values[k], 1000,.001,.1)
+    theta_values = stuff[0]/stuff[0][0]
+    t = stuff[1]
+    plt.plot(t,theta_values,label= f'euler med theta0 = {init_values[k]}')
+    
+    
+anal = analytisk(1,1000,.1,.001)
+plt.plot(anal[1],anal[0],label = "normalisert og linearisert analytisk")
+plt.title("Normaliserte numeriske løsninger vs linearisert analytisk")
+plt.xlabel("tid")
+plt.ylabel("Vinkelutslag i radianer")
+plt.grid()
+plt.legend()
 plt.show()
+
+
+
+#1.3 b
+def energi(theta_0, n,h,l,m):
+    w0 = np.sqrt(G/l)
+    theta = np.zeros(n)
+    theta[0]=theta_0 
+    theta_hjelp = np.zeros(n)
+
+    for i in range(1,len(theta)):
+        theta[i]=theta[i-1] + h*theta_hjelp[i-1]
+        theta_hjelp[i]=theta_hjelp[i-1]-h*(w0**2)*np.sin(theta[i-1])
+    t = np.arange(0, n*h, h)
+
+    U=m*G*l*(1-np.cos(theta))
+    K=.5*m*l*l*theta_hjelp**2
+    return U,K,t 
+
+
+tidssteg = [10**(-3),10**(-2),10**(-1)]
+
+for k in range(len(tidssteg)):
+    energiinfo = energi(.5,1000,tidssteg[k],.1,.1)
+    potensiell = energiinfo[0]
+    kinetisk = energiinfo[1]
+    t = energiinfo[2]
+    plt.plot(t,potensiell, label=f'Potensiell energi med tidssteg {tidssteg[k]}')
+    plt.plot(t,kinetisk, label=f'kinetisk energi med tidssteg {tidssteg[k]}')
+    totalenergi = potensiell+kinetisk
+    plt.plot(t,totalenergi, label=f'Total energi med tidssteg {tidssteg[k]}')
+plt.legend()
+plt.show()
+
+
+
+
